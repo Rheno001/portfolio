b7c108ee1d265f50d937ee861f8a0e2aed0fa3ed
Rheno001 <renfredreuben@gmail.com>
2025-05-22 22:07:27 +0100

    update

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -5,7 +5,16 @@ import Footer from '../../components/footer.jsx'
 import aboutme from '../../assets/aboutme.png'
 import knowme from '../../assets/Knowme.png'
 import experience from '../../assets/experience.png'
+import skilled from '../../assets/skilled.png'
 import topalbums from '../../assets/topalbums.png'
+import html from '../../assets/html.png'
+import css from '../../assets/css.png'
+import wp from '../../assets/wp.png'
+import git from '../../assets/git.png'
+import vs from '../../assets/vs.png'
+import tw from '../../assets/tw.png'
+import osint from '../../assets/osint.png'
+import projects from '../../assets/projects.png'
 
 function Index() {
   const imageRef = useRef(null);
@@ -41,6 +50,38 @@ function Index() {
       <section id="hero">
         <h1>RENFRED<br />REUBEN</h1>
       </section>
+
+      <section id="works"  className='py-[30px]'>
+        <div className='px-4 sm:px-6 lg:px-8'>
+          <img src={skilled} alt="Skilled In" />
+          <div className='frontend'>
+             <p className='text-[#EDFFFA] text-[15px] md:text-[20px] text-center mt-[30px] md:mt-[40px]'>Front-End Development Stack</p>
+             <ul className='flex items-center justify-center mt-[30px] gap-4'>
+              <li><img src={html} alt="html logo" /></li>
+              <li><img src={css} alt="css logo" /></li>
+              <li><img src={git} alt="git logo" /></li>
+              <li><img src={wp} alt="wordpress logo" /></li>
+              <li><img src={vs} alt="vs code logo" /></li>
+              <li><img src={tw} alt="tailwind logo" /></li>
+             </ul>
+          </div>
+          <div className='ceybersecurity'>
+             <p className='text-[#EDFFFA] text-[15px] md:text-[20px] text-center mt-[30px] md:mt-[40px]'>Cyber Security Stack</p>
+              <ul className='flex items-center justify-center mt-[30px] gap-4'>
+               <li><img src={osint} alt="html logo" /></li>
+               <li><img src={css} alt="css logo" /></li>
+               <li><img src={git} alt="git logo" /></li>
+              </ul>
+          </div>
+          <p className='text-center text-[#EDFFFA] bg-[#28312c] p-0 text-[15px] md:text-[20px] w-[70%] mx-auto mt-[30px] md:mt-[40px]'>Exploring the intersection of front-end development and cybersecurity. Designing with security in mind</p>
+        </div>
+
+        <div className='px-4 sm:px-6 lg:px-8 mt-[30px] md:mt-[100px]'>
+          <img src={projects} alt="projects" />
+        </div>
+      </section>
+
+
       <section id="about" className='py-[30px]'>
         <div className='px-4 sm:px-6 lg:px-8'>
         <img 
