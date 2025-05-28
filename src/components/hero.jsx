d107ac2da1f7fc2c28861cf5aa614075bdff1b63
Rheno001 <renfredreuben@gmail.com>
2025-05-28 14:25:40 +0100

    working on responsiveness

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -19,9 +19,9 @@ function Hero() {
       </div>
       <div className='flex flex-col items-center justify-center mt-[60px] relative px-4 sm:px-6 lg:px-31'>
         {/*<img src={moi} alt="Developers name" className='relative'/>*/}
-        <div className="desktop">
-           <h1 className='hidden md:block integral-font text-[#54625A] md:text-[160px] lg:text-[200px] font-black relative leading-40 mx-auto'>RENFRED</h1>
-           <h1 className='hidden md:block integral-font text-[#54625A] md:text-[160px] lg:text-[227px] font-black relative leading-40 mx-auto'>REUBEN</h1>
+        <div className="desktop bg-red-500">
+           <h1 className='hidden md:block integral-font text-[#54625A] md:text-[160px] lg:text-[200px] font-black relative leading-40 custom-tight mx-auto'>RENFRED</h1>
+           <h1 className='hidden md:block integral-font text-[#54625A] md:text-[160px] lg:text-[240px] font-black relative leading-40 custom-tight mx-auto'>REUBEN</h1>
         </div>
         <div className="mobile">
           <h1 className='block md:hidden integral-font text-[#54625A] text-[100px] font-black relative leading-20 mx-auto'>RENFRED- <br /> REUBEN</h1>
